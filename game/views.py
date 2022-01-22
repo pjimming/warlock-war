db97f58c4a9479face799e6c90edd8d667920c25
pan jiangming <2556323541@qq.com>
2022-01-22 18:43:57 +0800

    add game views

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align:center">PJM LOVE CXY</h1>'
+    image1 = '<img src="https://git.acwing.com/Payxtl/acapp/-/raw/master/game/photo.jpg" width=1800>'
+    return HttpResponse(line1 + image1)
