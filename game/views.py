53ef25fbc91bc7804e253cd9e40f25e3a45f4d2d
pan jiangming <2556323541@qq.com>
2022-01-27 12:01:06 +0800

    create project structure

--- game/views.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    line1 = '<h1 style="text-align:center">PJM LOVE CXY</h1>'
-    image1 = '<img src="https://git.acwing.com/Payxtl/acapp/-/raw/master/game/photo.jpg" width=1800>'
-    return HttpResponse(line1 + image1)
