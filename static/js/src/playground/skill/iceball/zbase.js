5f630a3e950fbce7623f0a4ebdfb64806d5a5c44
pan jiangming <2556323541@qq.com>
2022-05-16 17:05:17 +0800

    add bilibili video

---
+++ static/js/src/playground/skill/iceball/zbase.js
@@ -0,0 +1,25 @@
+class IceBall extends AcGameObject {
+    constructor(playground, player, x, y, radius, vx, vy, color, speed, move_length, damage) {
+        super();
+        this.playground = playground;
+        this.player = player;
+        this.ctx = this.playground.game_map.ctx;
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.vx = vx;
+        this.vy = vy;
+        this.color = color;
+        this.speed = speed;
+        this.move_length = move_length;
+        this.damage = damage;
+
+        this.eps = 0.01;
+    }
+
+    start() {
+    }
+
+    update() {
+    }
+}
